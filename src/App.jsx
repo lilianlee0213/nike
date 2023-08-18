88253547a5fbdb95f214b43038a2fbebd00625b1
Lilian Lee <lilianlee0213@gmail.com>
2023-08-18 17:52:41 -0400

    create and import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
+import {
+	Hero,
+	PopularProducts,
+	SuperQualtity,
+	Services,
+	SpecialOffer,
+	CustomerReviews,
+	Subscribe,
+	Footer,
+} from './sections/index';
 const App = () => {
 	return (
 		<main className="relative">
-			Nav
-			<section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-			<section className="padding">Popular Products</section>
-			<section className="padding-x py-10">Super Qualtity</section>
-			<section className="padding">Services</section>
-			<section className="padding">Special Offer</section>
-			<section className="bg-pale-blue padding">Customer Reviews</section>
-			<section className="padding-x sm:py-32 py 16 w-full">
-				Customer Reviews
+			{/* Nav */}
+			<section className="xl:padding-1 wide:padding-r padding-b">
+				<Hero />
 			</section>
-			<section className="bg-black padding-x padding-t pb-8">Footerr</section>
+			<section className="padding">
+				<PopularProducts />
+			</section>
+			<section className="padding-x py-10">
+				<SuperQualtity />
+			</section>
+			<section className="padding">
+				<Services />
+			</section>
+			<section className="padding">
+				<SpecialOffer />
+			</section>
+			<section className="bg-pale-blue padding">
+				<CustomerReviews />
+			</section>
+			<section className="padding-x sm:py-32 py 16 w-full">
+				<Subscribe />
+			</section>
+			<section className="bg-black padding-x padding-t pb-8">
+				<Footer />
+			</section>
 		</main>
 	);
 };
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQualtity from './SuperQualtity';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+	Hero,
+	PopularProducts,
+	SuperQualtity,
+	Services,
+	SpecialOffer,
+	CustomerReviews,
+	Subscribe,
+	Footer,
+};
