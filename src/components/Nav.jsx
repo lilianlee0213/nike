4d8d6d2a91ed5956babd01eb00df009e05ffe6a1
Lilian Lee <lilianlee0213@gmail.com>
2023-08-18 18:08:11 -0400

    add nav links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,26 @@
+import {headerLogo} from '../assets/images';
+import {navLinks} from '../constants';
 const Nav = () => {
-	return <div>Nav</div>;
+	return (
+		<header className="padding-x py-8 absolute z-10 w-full">
+			<nav className="flex justify-between max-container">
+				<a href="/">
+					<img src={headerLogo} alt="Logo" width={130} height={29} />
+				</a>
+				<ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+					{navLinks.map((link) => (
+						<li key={link.label}>
+							<a
+								href={link.href}
+								className="font-monts errat leading-normal text-lg text-slate-gray">
+								{link.label}
+							</a>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 export default Nav;
