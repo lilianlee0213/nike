178c43070331ae8e851ce6da8adf31bc62f7d8e1
Lilian Lee <lilianlee0213@gmail.com>
2023-08-18 17:40:08 -0400

    add sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+	return (
+		<main className="relative">
+			Nav
+			<section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+			<section className="padding">Popular Products</section>
+			<section className="padding-x py-10">Super Qualtity</section>
+			<section className="padding">Services</section>
+			<section className="padding">Special Offer</section>
+			<section className="bg-pale-blue padding">Customer Reviews</section>
+			<section className="padding-x sm:py-32 py 16 w-full">
+				Customer Reviews
+			</section>
+			<section className="bg-black padding-x padding-t pb-8">Footerr</section>
+		</main>
+	);
+};
+export default App;
