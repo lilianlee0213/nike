8415453bb8e4e4eebbaaa787380a87f025c15be6
Lilian Lee <lilianlee0213@gmail.com>
2023-08-21 20:37:42 -0400

    shoe img changes onClick

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -7,7 +7,9 @@ const ShoeCard = ({imgUrl, changeBigShoeImage, bigShoeImg}) => {
 	return (
 		<div
 			className={`border-2 rounded-xl ${
-				bigShoeImg === imgUrl ? 'border-coral-red' : 'border-transparent'
+				bigShoeImg === imgUrl.bigShoe
+					? 'border-coral-red'
+					: 'border-transparent'
 			} cursor-pointer max-sm:flex-1`}
 			onClick={handleClick}>
 			<div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,9 @@ import {arrowRight} from '../assets/icons';
 import {shoes, statistics} from '../constants';
 import {bigShoe1} from '../assets/images';
 import ShoeCard from '../components/ShoeCard';
+import {useState} from 'react';
 const Hero = () => {
+	const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
 	return (
 		<section
 			id="home"
@@ -38,7 +40,7 @@ const Hero = () => {
 			</div>
 			<div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-hero bg-cover bg-center ">
 				<img
-					src={bigShoe1}
+					src={bigShoeImg}
 					alt="show collection"
 					width={610}
 					height={500}
@@ -49,8 +51,10 @@ const Hero = () => {
 						<div key={index}>
 							<ShoeCard
 								imgUrl={shoe}
-								changeBigShoeImage={() => {}}
-								bigShoeImg=""
+								changeBigShoeImage={(shoe) => {
+									setBigShoeImg(shoe);
+								}}
+								bigShoeImg={bigShoeImg}
 							/>
 						</div>
 					))}
