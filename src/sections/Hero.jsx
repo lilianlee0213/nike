ed0ba584f330fca3ef1adf565e57e284f44d4924
Lilian Lee <lilianlee0213@gmail.com>
2023-08-22 18:13:31 -0400

    space btn "nike shoes"

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -19,8 +19,7 @@ const Hero = () => {
 						The New Arrival
 					</span>
 					<br />
-					<span className="text-coral-red inline-block mt-3">Nike</span>
-					Shoes
+					<span className="text-coral-red inline-block mt-3">Nike </span> Shoes
 				</h1>
 				<p className="text-slate-gray font-montserrat text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
 					Discover stylish Nike arrivals, quality comfort, and innovation for
