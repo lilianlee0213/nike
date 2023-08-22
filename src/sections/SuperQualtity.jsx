a6671ed5c3eb31fb366b8287a0a32f1ee59ad01e
Lilian Lee <lilianlee0213@gmail.com>
2023-08-21 21:21:48 -0400

    add shoe8 img

--- src/sections/SuperQualtity.jsx
+++ src/sections/SuperQualtity.jsx
@@ -1,3 +1,4 @@
+import {shoe8} from '../assets/images';
 import Button from '../components/Button';
 const SuperQualtity = () => {
 	return (
@@ -21,6 +22,15 @@ const SuperQualtity = () => {
 				<div className="mt-11 ">
 					<Button label="View details" />
 				</div>
+			</div>
+			<div className="flex flex-1 justify-center items-center">
+				<img
+					src={shoe8}
+					alt="shoe8"
+					width={570}
+					height={522}
+					className="object-contain"
+				/>
 			</div>
 		</section>
 	);
